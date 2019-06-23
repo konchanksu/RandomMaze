afa0d324c35a1d041f39192a7a46a4582778b808
konchanksu <nyankomi59@gmail.com>
2019-06-23 09:17:04 +0900

    少しの変更

--- Random_maze_ver0.1.c
+++ Random_maze_ver0.1.c
@@ -3,6 +3,19 @@
 #include<stdlib.h>
 #include<time.h>
 
+//プロトタイプ宣言
+int Opening(void);
+
 int main(){
+  HgOpen(1000,700);
+
+  
+
+  HgGetChar();
+  HgCLOSE();
   return 0;
+}
+
+int Opening(void){
+
 }
